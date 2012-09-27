87dabae4e64ea5037d5b32ac29db714e48ddaf5e
Georgy Nepotachev <g.nepotachev@gmail.com>
2012-09-27 09:38:04 +0400

    Minor changes to show the list of products (not finished yet)

--- protected/views/productcategory/view.php
+++ protected/views/productcategory/view.php
@@ -1,9 +1,16 @@
 <?php
 $this->pageTitle=$model->name;
-if($this->beginCache('fullcatalog-'.$model->id, array('duration'=>60))) {
+if($this->beginCache('catalog-'.$model->id, array('duration'=>60))) {
 	$children = $model->children()->findAll();
 	$this->widget('application.components.Categories', array(
 	  'categories' => $children
+	));
+	$this->endCache();
+}
+if($this->beginCache('productlist-'.$model->id, array('duration'=>60))) {
+	$products = Product::model()->findAllByAttributes(array('categoryid'=>$model->id));
+	$this->widget('application.components.ProductList', array(
+	  'products' => $products
 	));
 	$this->endCache();
 }
